df69dc8e3cc5ef494b5e61bfa8d16e50c5147205
Tranzate <119891614+Tranzate@users.noreply.github.com>
2024-12-13 13:58:36 +0100

    add EaseCubicCurves

---
+++ Curve Exo/Assets/Scripts/Utils/EaseCubicCurves.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EaseCubicCurves
+{
+    /// <summary>
+    /// EaseInCubic function
+    /// Smoothly accelerates the beginning by gradually increasing the interpolated value
+    /// </summary>
+    /// <param name="_t">Normalized value beetwen 0 and 1 for time progress</param>
+    /// <returns>Interpolated value</returns>
+    static float EaseInCubic(float _t)
+    {
+        return _t * _t * _t;
+    }
+
+    /// <summary>
+    /// EaseOutCubic function
+    /// Smoothly decelerates the end by gradually slowing down the interpolated value.
+    /// </summary>
+    /// <param name="_t">Normalized value beetwen 0 and 1 for time progress</param>
+    /// <returns>Interpolated value</returns>
+    static float EaseOutCubic(float _t)
+    {
+        return 1 - Mathf.Pow(1 - _t, 3);
+    }
+
+    /// <summary>
+    /// EaseInOutCubic
+    /// Combine EaseInCubic and EaseOutCubic, that starts slowly, accelerates in the middle, and then decelerates toward the end
+    /// </summary>
+    /// <param name="_t">Normalized value beetwen 0 and 1 for time progress</param>
+    /// <returns>Interpolated value</returns>
+    static float EaseInOutCubic(float _t)
+    {
+        return _t < 0.5f ? 4 * _t * _t * _t : 1 - Mathf.Pow(-2 * _t + 2, 3) / 2;
+    }
+
+}
